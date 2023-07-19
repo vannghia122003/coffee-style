6a73898900dbe7fb6d98a7762bd5d07185819543
Tran Van Nghia <vannghia.122003@gmail.com>
2023-07-19 12:42:11 +0700

    Add header.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./public/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                'light-coffee': '#C89f94',
+            },
+        },
+    },
+    plugins: [],
 }
-
