3362c16341fd5a90917a0c10274b628f8c91028c
Tran Van Nghia <vannghia.122003@gmail.com>
2023-07-23 12:42:26 +0700

    Add parallax section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ['./public/**/*.{html,js}'],
     theme: {
+        screens: {
+            'xs': '480px',
+            ...defaultTheme.screens,
+        },
         extend: {
-            screens: {
-                'xs': '480px'
-            },
             fontFamily: {
                 Karla: ['Karla', 'sans-serif'],
             },
